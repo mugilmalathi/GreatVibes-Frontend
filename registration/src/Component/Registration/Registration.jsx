a10b464f39a4dd9e7fe53fa2a77aabbe828fd2b9
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-29 01:10:04 +0530

    validatio done

--- registration/src/Component/Registration/Registration.jsx
+++ registration/src/Component/Registration/Registration.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-
 const Registration = () => {
 
     const [formData, setFormData] = useState({
@@ -18,6 +17,7 @@ const Registration = () => {
   
     const handleSubmitter = (e)=>{
 
+      e.preventDefault()
       if(formData.name == ""){
         return seterrorMessage("Please Enter the Name")
       }
@@ -40,6 +40,7 @@ const Registration = () => {
         return seterrorMessage("Please Enter the Message")
       }
       alert(`Hello ${formData.name}, you have registred successfully..!`)
+      
     
         axios.post("http://localhost:8080/users", formData)
         .then(()=>{
@@ -66,90 +67,91 @@ const Registration = () => {
   return (
     <div id='register'>
         <div id='top_div'>
-            <h1>Registration Form</h1>
-            {errorMessage.length > 0 && <div style={{ marginBottom: '10px', color: 'red' }}>{errorMessage}</div>}
-            <input 
-              id='name' 
-              type="text" 
-              placeholder='Enter Name' 
-              onChange={handleChange}
-              value={formData.name}
-              required
-            />
-            <br />
-            <input 
-              id='email' 
-              type="text" 
-              placeholder='Enter Email' 
-              onChange={handleChange}
-              value={formData.email}
-              required
-            />
-            <br />
-            <input 
-              id='mobile' 
-              type="number" 
-              placeholder='Enter Mobile Number' 
-              onChange={handleChange}
-              value={formData.mobile}
-            />
-            <br />
-            <select 
-                id="country" 
-                onChange={handleChange}
-                value={formData.country}
-            >
-              <option value="">Select Country</option>
-              <option value="India">India</option>
-              <option value="Australia">Australia</option>
-              <option value="US">United States</option>
-              <option value="China">China</option>
-              <option value="Japan">Japan</option>
-              <option value="UK">United Kingdom</option>
-              <option value="Srilanka">Srilanka</option>
-            </select>
-            <br />
-            <select 
-                id="state" 
-                onChange={handleChange}
-                value={formData.state}
-            >
-              <option value="">Select State</option>
-              <option value="Tamilnadu">Tamilnadu</option>
-              <option value="Andhra">Andhra Pradesh</option>
-              <option value="Kerala">Kerala</option>
-              <option value="Delhi">Delhi</option>
-              <option value="Karnataka">Karnataka</option>
-              <option value="Mumbai">Mumbai</option>
-              <option value="Rajastan">Rajastan</option>
-            </select>
-            <br />
-            <select 
-                id="city" 
-                onChange={handleChange}
-                value={formData.city}
-            >
-              <option value="">Select City</option>
-              <option value="Tamilnadu">Chennai</option>
-              <option value="Andhra">Salem</option>
-              <option value="Kerala">Erode</option>
-              <option value="Delhi">Dharmapuri</option>
-              <option value="Karnataka">Coimbatore</option>
-              <option value="Mumbai">Kanyakumari</option>
-              <option value="Rajastan">Trichi</option>
-            </select>
-            <br />
-            <textarea 
-              id="message"
-              placeholder='Enter Message Here'
-              onChange={handleChange}
-              value={formData.message}
-            ></textarea>
-            <br />
-            <button 
-              id='submit'
-              onClick={handleSubmitter}
-            >REGISTER</button>
+            <form onSubmit={handleSubmitter}>
+                <h1>Registration Form</h1>
+                {errorMessage.length > 0 && <div style={{ marginBottom: '10px', color: 'red' }}>{errorMessage}</div>}
+                <input 
+                  id='name' 
+                  type="text" 
+                  placeholder='Enter Name' 
+                  onChange={handleChange}
+                  value={formData.name}
+                  required
+                />
+                <br />
+                <input 
+                  id='email' 
+                  type="text" 
+                  placeholder='Enter Email' 
+                  onChange={handleChange}
+                  value={formData.email}
+                  required
+                />
+                <br />
+                <input 
+                  id='mobile' 
+                  type="number" 
+                  placeholder='Enter Mobile Number' 
+                  onChange={handleChange}
+                  value={formData.mobile}
+                />
+                <br />
+                <select 
+                    id="country" 
+                    onChange={handleChange}
+                    value={formData.country}
+                >
+                  <option value="">Select Country</option>
+                  <option value="India">India</option>
+                  <option value="Australia">Australia</option>
+                  <option value="US">United States</option>
+                  <option value="China">China</option>
+                  <option value="Japan">Japan</option>
+                  <option value="UK">United Kingdom</option>
+                  <option value="Srilanka">Srilanka</option>
+                </select>
+                <br />
+                <select 
+                    id="state" 
+                    onChange={handleChange}
+                    value={formData.state}
+                >
+                  <option value="">Select State</option>
+                  <option value="Tamilnadu">Tamilnadu</option>
+                  <option value="Andhra">Andhra Pradesh</option>
+                  <option value="Kerala">Kerala</option>
+                  <option value="Delhi">Delhi</option>
+                  <option value="Karnataka">Karnataka</option>
+                  <option value="Mumbai">Mumbai</option>
+                  <option value="Rajastan">Rajastan</option>
+                </select>
+                <br />
+                <select 
+                    id="city" 
+                    onChange={handleChange}
+                    value={formData.city}
+                >
+                  <option value="">Select City</option>
+                  <option value="Tamilnadu">Chennai</option>
+                  <option value="Andhra">Salem</option>
+                  <option value="Kerala">Erode</option>
+                  <option value="Delhi">Dharmapuri</option>
+                  <option value="Karnataka">Coimbatore</option>
+                  <option value="Mumbai">Kanyakumari</option>
+                  <option value="Rajastan">Trichi</option>
+                </select>
+                <br />
+                <textarea 
+                  id="message"
+                  placeholder='Enter Message Here'
+                  onChange={handleChange}
+                  value={formData.message}
+                ></textarea>
+                <br />
+                <button 
+                  id='submit'
+                >REGISTER</button>
+              </form>
         </div>
         <div id='bottom_div'></div> 
     </div>
